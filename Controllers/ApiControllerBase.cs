82c4faea148cce52acdc5e32e9702a06c45e13d5
JakubKakol <kakoljakub7@gmail.com>
2023-05-06 01:09:44 +0200

    Update exception handling helper methods
    Add JournalItem model, create an instance of it when exception is caught in TryCatchResultAsync or TryCatchOperationAsync and use it to form a response message.

--- Controllers/ApiControllerBase.cs
+++ Controllers/ApiControllerBase.cs
@@ -24,14 +24,10 @@ namespace BackendTestTask.Controllers
                 var result = await action.Invoke();
                 return Ok(result);
             }
-            //TODO - Add custom exception and handling
-            //catch (SecureException ex)
-            //{
-
-            //}
             catch (Exception ex)
             {
-                return StatusCode(500, ex.Message); //TODO - Customize message
+                var journalItem = await createJournalItemAsync(ex);
+                return StatusCode(500, getErrorResult(journalItem));
             }
         }
 
@@ -42,17 +38,58 @@ namespace BackendTestTask.Controllers
             {
                 await action.Invoke();
             }
-            //TODO - Add custom exception and handling
-            //catch (SecureException ex)
-            //{
-
-            //}
             catch (Exception ex)
             {
-                return StatusCode(500, ex.Message); //TODO - Customize message
+                var journalItem = await createJournalItemAsync(ex);
+                return StatusCode(500, getErrorResult(journalItem));
             }
 
             return Ok();
+        }
+
+        private async Task<JournalItem> createJournalItemAsync(Exception exception)
+        {
+            var request = _httpContextAccessor.HttpContext.Request;
+            var queryString = request.QueryString.ToString();
+            var requestBody = await getRequestBodyAsync(request);
+
+            var journalItem = new JournalItem
+            {
+                EventId = Guid.NewGuid().ToString(),
+                Timestamp = DateTimeOffset.UtcNow, //TODO - Date format
+                RequestQueryString = queryString,
+                RequestBody = requestBody,
+                ExceptionType = exception.GetType().Name, //TODO - Exception type name adjustment
+                ExceptionMessage = exception.Message,
+                StackTrace = exception.StackTrace
+            };
+
+            //TODO - Add DbSet for JournalItem
+
+            return journalItem;
+        }
+
+        private async Task<string> getRequestBodyAsync(HttpRequest request)
+        {
+            request.EnableBuffering();
+            var encoding = Encoding.UTF8;
+            using var reader = new StreamReader(request.Body, encoding, detectEncodingFromByteOrderMarks: true, bufferSize: 1024, leaveOpen: true);
+            var requestBody = await reader.ReadToEndAsync();
+            request.Body.Position = 0;
+            return requestBody;
+        }
+
+        private object getErrorResult(JournalItem item)
+        {
+            return new
+            {
+                type = item.ExceptionType,
+                id = item.EventId,
+                data = new
+                {
+                    message = item.ExceptionType == "Secure" ? item.ExceptionMessage : $"Internal server error ID = {item.EventId}" //TODO - Remove workaround
+                }
+            };
         }
     }
 }
---
+++ Data/Models/JournalItem.cs
@@ -0,0 +1,14 @@
+﻿namespace BackendTestTask.Data.Models
+{
+    public class JournalItem
+    {
+        public int ID { get; set; }
+        public string EventId { get; set; }
+        public DateTimeOffset Timestamp { get; set; }
+        public string RequestQueryString { get; set; }
+        public string RequestBody { get; set; }
+        public string ExceptionType { get; set; }
+        public string ExceptionMessage { get; set; }
+        public string StackTrace { get; set; }
+    }
+}
