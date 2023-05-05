f06780f709abab558a6b96fcaa4d3ad5275aaadb
JakubKakol <kakoljakub7@gmail.com>
2023-05-05 13:50:31 +0200

    Create TreeController.cs
    Add controller inheriting from ApiControllerBase and providing Get method which returns Json of tree and related nodes.

---
+++ Controllers/TreeController.cs
@@ -0,0 +1,33 @@
+﻿using BackendTestTask.Data.DbContexts;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BackendTestTask.Controllers
+{
+    public class TreeController : ApiControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public TreeController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Get(string name)
+        {
+            try
+            {
+                var tree = _context.Tree
+                    .Include(t => t.Children)
+                    .First(t => t.Name == name);
+
+                return Json(tree);
+            }
+            catch (Exception ex)
+            {
+                //TODO - Add custom exception and handling
+                return Json(ex);
+            }
+        }
+    }
+}
