fb9bcaad972c60f1115cc58d6218224970ec1112
JakubKakol <kakoljakub7@gmail.com>
2023-05-05 10:37:15 +0200

    Set up Entity Framework
    Install required packages, prepare DefaultConnection connection string and create ApplicationDbContext.

---
+++ Data/DbContexts/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BackendTestTask.Data.DbContexts
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
